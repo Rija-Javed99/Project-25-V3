93071bbb1b8d734fad61c086fa88bf4e48ad6ff5
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-05-29 13:47:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
-var dustbinObj, paperObject,groundObject	
+
 var world;
 
 
@@ -16,10 +16,11 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 	
-	paperObject=new paper(200,450,70);
-	groundObject=new ground(width/2,670,width,20);
-	dustbinObj=new dustbin(1200,650);
-	//Create a Ground
+	
+	
+	
+	
+	
 	
 
 	var render = Render.create({
@@ -42,14 +43,9 @@ function draw() {
   rectMode(CENTER);
   background(230);
  
-  
-  paperObject.display();
-  groundObject.display();
-  dustbinObj.display();
 
-  
-  
- 
+	
+	
   
   
  
@@ -58,7 +54,9 @@ function draw() {
 function keyPressed() {
   	if (keyCode === UP_ARROW) {
 
-    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
+    
+		
+		
 
     
   	}
